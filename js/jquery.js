6d509c2366da80cf686c924bcf85c69984ab23f3
shashishashank6 <shashi.shashank6@gmail.com>
2017-03-31 06:29:19 -0700

    update-3

---
+++ js/jquery.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+ 
+  $(".owl-demo").owlCarousel({
+ 
+      pagination:false,
+      items : 4,
+      navigation:true,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+ 
+  });
+ 
+});
